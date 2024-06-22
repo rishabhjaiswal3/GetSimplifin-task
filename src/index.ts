d19005ce8499755d49dd6cd6bcc24ea7af0c597f
Rishabh Jaiswal <rishabh.jaiswal@mogiio.com>
2024-06-22 06:44:16 +0530

    added little comments

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ class RideSharingApp {
   private vehicles: { [key: string]: Vehicle } = {};
   private rides: Ride[] = [];
 
+  // add users
   add_user(_user) {
     const isUserExisted = this.users.filter(
       (user) => user?.userId == _user?.userId
@@ -23,6 +24,7 @@ class RideSharingApp {
     return `Success:: user created with userId ${_user?.userId}`;
   }
 
+  // add vehicles
   add_vehicle(_vehicle) {
     let plateNumber = _vehicle?.plateNumber;
     if(!plateNumber) {
@@ -35,6 +37,7 @@ class RideSharingApp {
     return `Success:: vehicle added with plate number ${plateNumber}`;
   }
 
+  // we can offer a ride 
   offer_ride(_ride) {
     const _sameOfferedVehicle = this.rides.filter((ride) => {
       return (
@@ -49,6 +52,7 @@ class RideSharingApp {
     return `Success:: this vehicle with plate number ${_ride.vehiclePlateNumber} started to offer`;
   }
 
+  // we can end the ride by giving the ride information
   end_ride(_ride) {
     const _rideIndex = this.rides.findIndex(
       (ride) => ride.rideId === _ride.rideId
@@ -60,6 +64,7 @@ class RideSharingApp {
     return `Failed:: ride with id ${_ride.rideId} not existed with active Status`;
   }
 
+  // find the direct route for the user from origin to destination
   find_direct_route(
     _startOrigin,
     _endDestination,
@@ -99,6 +104,7 @@ class RideSharingApp {
     return selectedRide;
   }
 
+  // increase the count of rides taken and rider offered
   increase_count(userId,offeredBy) {
 
     this.users.forEach((user) => {
@@ -112,6 +118,7 @@ class RideSharingApp {
 
   }
 
+  // select the rides for the user from offered rides
   select_ride(selectRide) {
     
     let { startOrigin, endDestination, seats, most_vacant = false, preferredVehicle = null, userId} = selectRide;  
@@ -131,6 +138,7 @@ class RideSharingApp {
     return;
   }
 
+  // print user name, rides taken and rides offered count
   print_users() {
     console.log("=================================== My All Users ==================================")
     this.users.forEach((user)=>{
@@ -139,6 +147,7 @@ class RideSharingApp {
     console.log("=================================== End ==================================")
   }
 
+  // print rides and it's information
   print_ride_stats() {
     console.log("=================================== My Rides ==================================")
     this.rides.forEach((ride) =>{
